510a596b23df91b1604067441130fcb3745eb833
Khagesh Sharma <khageshsharma24@gmail.com>
2024-03-05 14:37:28 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="flex w-[100vw] h-[100vh] items-center justify-center">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
